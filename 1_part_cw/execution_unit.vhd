9501974b6efcfd11e5529e81b926b97db295197b
James <james@james-VirtualBox.(none)>
2013-02-02 01:55:16 +0000

    more work done on 02/02

--- 1_part_cw/execution_unit.vhd
+++ 1_part_cw/execution_unit.vhd
@@ -47,6 +47,9 @@ architecture syn of execution_unit is
   signal next_io_out: byte_vector((ports_out - 1) downto 0) := (others => byte_null);
   
   signal internal_opcode: std_logic_vector(7 downto 0) := (others => 'X');
+  signal next_test_opcode: std_logic_vector(7 downto 0) := (others => 'X');
+  signal curr_test_opcode: std_logic_vector(7 downto 0) := (others => 'X');
+  
   
   signal io_out_port: std_logic_vector(7 downto 0) := (others => 'X');
   signal and_argument: std_logic_vector(7 downto 0) := (others => 'X');
@@ -65,21 +68,21 @@ architecture syn of execution_unit is
   signal test_flag: std_logic := 'X';
   signal next_test_flag: std_logic := 'X';
   
-  signal whee: std_logic := '0';
-  
   signal delay: std_logic := '1';
   signal next_delay: std_logic := '1';
   
-  signal lhs: std_logic_vector(7 downto 0);
+  
+  --signal lhs: std_logic_vector(7 downto 0);
   
   --signal left_hand_side: std_logic_vector(byte_vector(((ports_in - 1) downto 0)(to_integer(unsigned(std_logic_vector(7 downto 0)))))) := (others => 'X');
   
   
   
 begin
+rom_en <= '1';
 io_out <= curr_io_out;	
-rom_addr <= std_logic_vector(curr_test_pc);
-test_pc <= (unsigned(curr_test_pc )	);
+rom_addr <= std_logic_vector(next_test_pc);
+test_pc <= (unsigned(curr_test_pc)	);
 test_opcode <= internal_opcode;
 internal_opcode <= rom_data(31 downto 24);
 io_out_port <= rom_data(23 downto 16);
@@ -87,6 +90,7 @@ and_argument <= rom_data(15 downto 8);
 xor_argument <= rom_data(7 downto 0);
 internal_test_ins_data <= rom_data(23 downto 0);
 test_ins_data <= internal_test_ins_data;
+
   
   
   process(clk, rst)
@@ -107,80 +111,77 @@ test_ins_data <= internal_test_ins_data;
 			curr_io_out <= next_io_out;
 			curr_test_pc <= next_test_pc;
 			first_inst <= next_first_inst;
-			rom_en <= '1';
+			--rom_en <= '1';
 			test_flag <= next_test_flag;
 			delay <= next_delay;
 			
-			--if(delay = '1') then
-			--	curr_test_pc <= "000000";
-			--	delay <= '0';
-  		
-  		--end if;
+			if(delay = '1') then
+			
+				curr_test_pc <= "000000";
+				next_delay <= '0';
+				
+  		end if;
 			
 		end if;
   
   end process;
+  		
+
   
-  process(rom_data, internal_opcode, curr_io_out, io_out_port, and_argument, xor_argument, curr_test_pc)
-  
-  	--variable lhs: std_logic_vector(7 downto 0);
-  
+  process(rst, test_flag, internal_test_ins_data, curr_io_out, io_out_port, and_argument, xor_argument, curr_test_pc)
   begin
   
-  	
-  
-  	if (internal_opcode = "00000000") then --IUC
-  		--stuff
-  		
-  	elsif (internal_opcode = "00000001") then --HUC
-  		--stuff
-  		
-  	elsif (internal_opcode = "00000010") then --BUC
-  		--stuff
-  		
-  	elsif (internal_opcode = "00000011") then --BIC
-			
-			if(test_flag = '1') then
-																									--std_logic_vector((n_bits(rom_size) - 1) downto 0)
-				next_test_pc <= std_logic_vector(internal_test_ins_data((n_bits(rom_size) - 1) downto 0));
+			next_test_pc <= curr_test_pc;
+			next_io_out <= curr_io_out;
+			next_test_flag <= test_flag;
+		
+			if (internal_opcode = "00000000") then --IUC
+				--stuff
 				
-			elsif(test_flag = '0') then
+			elsif (internal_opcode = "00000001") then --HUC
+				--stuff
+				
+			elsif (internal_opcode = "00000010") then --BUC
+				--stuff
+				
+			elsif (internal_opcode = "00000011") then --BIC
+			
+				if(test_flag = '1') then
+																								
+					next_test_pc <= std_logic_vector(internal_test_ins_data((n_bits(rom_size) - 1) downto 0));
+				
+				elsif(test_flag = '0') then
+
+					next_test_pc <= std_logic_vector(unsigned(curr_test_pc) + 1);
+				
+				end if;
+			
+				next_test_flag <= 'X';
+				
+			elsif (internal_opcode = "00000100") then --SET0
+				next_io_out(to_integer(unsigned(io_out_port))) <= std_logic_vector((curr_io_out(to_integer(unsigned(io_out_port)))) and and_argument) xor xor_argument;
+			  next_test_pc <= std_logic_vector(unsigned(curr_test_pc) + 1);  	  
+				
+			elsif (internal_opcode = "00000101") then --TSTI
 			
 				next_test_pc <= std_logic_vector(unsigned(curr_test_pc) + 1);
+
+				if ((std_logic_vector((io_in(to_integer(unsigned(io_out_port))) and and_argument) xor xor_argument)) = "00000000") then --It is zero, branch
+			
+					next_test_flag <= '1';
+				
+				else --Don't branch
+			
+					next_test_flag <= '0';
+			
+				end if;
 				
 			end if;
 			
-			--next_test_flag <= 'X';
-  		
-  	elsif (internal_opcode = "00000100") then --SET0
-  		next_io_out(to_integer(unsigned(io_out_port))) <= std_logic_vector((curr_io_out(to_integer(unsigned(io_out_port)))) and and_argument) xor xor_argument;
-  	  next_test_pc <= std_logic_vector(unsigned(curr_test_pc) + 1);  	  
-  		
-  	elsif (internal_opcode = "00000101") then --TSTI
-  		lhs <= std_logic_vector((io_in(to_integer(unsigned(io_out_port))) and and_argument) xor xor_argument);
-  		if (lhs = "00000000") then --It is zero, branch
+			if(rst = '1') then
+				next_test_pc <= "000000";
+			end 
 			
-				whee <= '0';
-				next_test_flag <= '1';
-				
-			else --Don't branch
-			
-				whee <= '1';
-				next_test_flag <= '0';
-			
-			end if;
-  		
-  	end if;
-  	
-  	
-  	
-  	--if(first_inst = '1') then
-  		
-  	--	next_test_pc <= "000000";
-  	--	next_first_inst <= '0';
-  		
-  	--end if;
-  
   end process;
   
   
